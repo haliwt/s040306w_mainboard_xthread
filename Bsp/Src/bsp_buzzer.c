b12d3935c34faafb25176304972233cb13eca654
haliwt <jonewang@126.com>
2026-04-22 11:33:00 +0800

    update

--- Bsp/Src/bsp_buzzer.c
+++ Bsp/Src/bsp_buzzer.c
@@ -1,5 +1,8 @@
 #include "bsp.h"
 
+static void beep_delay_us(uint32_t us) ;
+static void beep_delay_ms(uint32_t ms) ;
+
 
 void tim3_buzzer_sound_config(void)
 {
@@ -13,10 +16,12 @@ void tim3_buzzer_sound_config(void)
 
 static void tim3_stop_buzzer_sound_config(void)
 {
+
+  
+  LL_TIM_OC_SetCompareCH4(TIM3,0);
   LL_TIM_DisableCounter(TIM3);
   LL_TIM_CC_DisableChannel(TIM3,LL_TIM_CHANNEL_CH4);
-	LL_TIM_OC_SetCompareCH4(TIM3,0);
-
+	
 
 }
 
@@ -27,8 +32,10 @@ void buzzer_sound_once(void)
 {
    
    tim3_buzzer_sound_config();
-   LL_mDelay(1);//tx_thread_sleep(1);
+   //LL_mDelay(1);//tx_thread_sleep(1);
+   delay_ms(20);
    tim3_stop_buzzer_sound_config();
+   LL_TIM_DisableCounter(TIM3);
 
 }
 
@@ -43,6 +50,33 @@ void buzzer_sound(void)
    tx_thread_sleep(20);
    tim3_stop_buzzer_sound_config();
 
+}
+
+
+/**
+ * @brief  微秒级延时 (粗略)
+ * @param  us: 延时微秒数
+ * @note   64MHz 下, 1us 大约需要 64 个周期
+ * 假设 while 循环 + NOP 占用 4 个周期, 则需要循环 16 次
+ */
+static void beep_delay_us(uint32_t us) 
+{
+    // 这里的 16 是基于 64MHz 的经验估算值，可能需要根据编译器优化等级微调
+    uint32_t count = us * 16; 
+    while (count--) {
+        __NOP();
+    }
+}
+
+/**
+ * @brief  毫秒级延时
+ * @param  ms: 延时毫秒数
+ */
+static void beep_delay_ms(uint32_t ms) 
+ {
+    while (ms--) {
+        delay_us(1000);
+    }
 }
 
 
