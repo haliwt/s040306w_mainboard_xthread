37196f77c7898a783ee777384121edbc1c1b58d5
haliwt <jonewang@126.com>
2026-06-10 14:15:55 +0800

    update

--- Core/Src/iwdg.c
+++ Core/Src/iwdg.c
@@ -29,23 +29,25 @@ void MX_IWDG_Init(void)
 {
 
   /* USER CODE BEGIN IWDG_Init 0 */
-  /* T-over = (prev * reload)/32KHz   */
+  /* T-over = (prev * (reload + 1))/32KHz   */
   /* USER CODE END IWDG_Init 0 */
+  //Counter Value between Min_Data=0 and Max_Data=0x0FFF
 
   /* USER CODE BEGIN IWDG_Init 1 */
    // iwdg = 2s Time_out = (relaod * prescaler)/32000 = s.
   /* USER CODE END IWDG_Init 1 */
-  LL_IWDG_Enable(IWDG);
+  LL_IWDG_Enable(IWDG);//error is 
   LL_IWDG_EnableWriteAccess(IWDG);
-  LL_IWDG_SetPrescaler(IWDG, LL_IWDG_PRESCALER_32);
-  LL_IWDG_SetReloadCounter(IWDG, 8000);//8000*(32/32000)=8s
+  LL_IWDG_SetPrescaler(IWDG, LL_IWDG_PRESCALER_64);
+  LL_IWDG_SetReloadCounter(IWDG, 3999);//4000*(64/32000)=8s//4095
   while (LL_IWDG_IsReady(IWDG) != 1)
   {
   }
 
   LL_IWDG_ReloadCounter(IWDG);
   /* USER CODE BEGIN IWDG_Init 2 */
-
+  // 6. 最后一步：正式使能/启动看门狗
+   LL_IWDG_Enable(IWDG);
   /* USER CODE END IWDG_Init 2 */
 
 }
