e411aa44d7a0655d037f5d0232d028c272d4dc4f
haliwt <jonewang@126.com>
2026-06-12 07:50:51 +0800

    update

--- Bsp/Src/bsp_time.c
+++ Bsp/Src/bsp_time.c
@@ -59,7 +59,6 @@ void works_run_two_hours_state(void)
   if(gpro_t.stopTwoHours_flag ==1){//WT.EDIT 2025.11.19
 		 two_hours_f = 1;
 
-         
          PLASMA_SetLow(); //
          PTC_SetLow();
          ultrasonic_close();
